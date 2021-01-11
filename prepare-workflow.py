e690c0e1916f30987a033277434a04a6d2d55be7
cegielskir <cegielski.rafaal@gmail.com>
2021-01-11 19:52:53 +0100

    Add convert script

---
+++ prepare-workflow.py
@@ -0,0 +1,26 @@
+import json
+
+with open('./workflow.json') as json_file:
+    data = json.load(json_file)
+    
+processes = data['processes']
+    
+for process in processes:
+    all_args = ''
+    executor = process['config']['executor']
+    for arg in executor['args']:
+        all_args += ' ' + arg
+        
+    if '>' in all_args or '|' in all_args or '*' in all_args:
+        source_exe = executor['executable']
+        executor['executable'] = 'sh'
+        new_args = []
+        new_args.append('-c')
+        new_args.append(source_exe + ' ' + all_args)
+
+        executor['args'] = new_args
+        
+data['processes'] = processes
+
+with open('workflow.json', 'w') as result_json:
+    data = json.dump(data, result_json, indent=4)
